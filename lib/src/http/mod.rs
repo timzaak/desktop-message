ad6ea26f1db34368aafc29c88a09ce898d6d1ee5
timzaak <zsy.evan@gmail.com>
2025-05-19 10:50:54 +0800

    add basic path

--- lib/src/http/mod.rs
+++ lib/src/http/mod.rs
@@ -2,14 +2,20 @@ use std::net::SocketAddr;
 use salvo::prelude::*;
 use anyhow::Result;
 use salvo::conn::tcp::TcpAcceptor;
+use once_cell::sync::OnceCell;
+static BasicPath: OnceCell<String> = OnceCell::new();
 #[handler]
 async fn hello_world(req:&mut Request, res: &mut Response) {
     let path = req.query::<String>("path");
-    match path {
-        Some(p)=> {
-            res.send_file(p, req.headers()).await;
+    match (path, BasicPath.get()) {
+        (Some(p), Some(prefix))=> {
+            res.send_file(format!("{}/{}", prefix, p), req.headers()).await;
         }
-        None => {
+        (_, None) => {
+            res.status_code(StatusCode::BAD_REQUEST);
+            res.render("Basic Path does not config");
+        }
+        (None,_) => {
             res.status_code(StatusCode::NOT_FOUND);
             res.render("");
         }
@@ -19,6 +25,9 @@ pub struct HttpServer {
 }
 
 impl HttpServer {
+    pub fn set_basic_path(path: String) -> bool{
+        BasicPath.set(path).is_ok()
+    }
     pub fn try_bind(address: SocketAddr) -> Result<(TcpAcceptor, SocketAddr)> {
         let acceptor = tokio::runtime::Handle::current().block_on(async {
             let acceptor = TcpListener::new(address).try_bind().await;
@@ -33,5 +42,4 @@ impl HttpServer {
         Server::new(acceptor).serve(router).await;
         Ok(())
     }
-    
 }
--- lib/src/server.rs
+++ lib/src/server.rs
@@ -21,6 +21,7 @@ impl Server {
         let (mqtt_address, mqtt_listener) = mqtt::MqttServer::try_bind("0.0.0.0:1883".parse::<SocketAddr>()?)?;
         let mqtt_handler = tokio::spawn(async { mqtt::MqttServer::start_rmqtt_server(mqtt_listener).await });
         let http_handler = tokio::spawn(async { HttpServer::start_http_server(acceptor).await });
+        HttpServer::set_basic_path("/".to_owned());
         let handler = try_join_all(vec![mqtt_handler, http_handler]);
         Ok(Server {
             handler,
