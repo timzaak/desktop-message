822a42cd832e92f074749eec981e4937fe2fed7f
timzaak <zsy.evan@gmail.com>
2025-07-06 20:12:37 +0800

    update deps

--- deskmsg/src/mqtt/mod.rs
+++ deskmsg/src/mqtt/mod.rs
@@ -22,14 +22,14 @@ impl MqttServer {
         let mut plugin_config = HashMap::new();
         plugin_config.insert("auto-subscription".to_owned(), r#"subscribes = []"#.to_owned());
 
-        let scx = ServerContext::new() /*.plugins_config_map(plugin_config)*/
+        let scx = ServerContext::new().plugins_config_map(plugin_config)
             .build()
             .await;
 
         acl::register_named(&scx, "acl", true, false).await?;
         // subscribe server client connection $SYS/brokers/+/clients/+/#
         rmqtt_sys_topic::register_named(&scx, "sys-topic", true, false).await?;
-        //rmqtt_auto_subscription::register_named(&scx, "auto-subscription", true, false).await?;
+        rmqtt_auto_subscription::register_named(&scx, "auto-subscription", true, false).await?;
 
         RMqttServer::new(scx).listener(listener).build().run().await?;
         Ok(())
